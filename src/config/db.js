66e08d6308737ac6e134cdd63951d8a0fc4853e4
A M Sadat <sadat77330@gmail.com>
2025-10-16 05:44:52 +0600

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,11 @@
-
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI || 'mongodb+srv://itfirmplus_db_user:KbdhadMZ4CWvKn35@itfirmplus.icxoi9v.mongodb.net/?retryWrites=true&w=majority&appName=itfirmplus', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        const uri = process.env.MONGO_URI || 
+            'mongodb+srv://itfirmplus_db_user:KbdhadMZ4CWvKn35@itfirmplus.icxoi9v.mongodb.net/?retryWrites=true&w=majority&appName=itfirmplus';
+
+        await mongoose.connect(uri);
         console.log('✅ MongoDB connected successfully!');
     } catch (err) {
         console.error(`❌ Error connecting to MongoDB: ${err.message}`);
